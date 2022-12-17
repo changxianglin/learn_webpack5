fafe69c61e49b3a14d71a83c4ea80be49ad73c0b
changxianglin <281532612@qq.com>
2022-12-17 16:25:15 +0800

    add html teamplate and plugin

--- dist/main.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";console.log("I don't trust staris. They're always up to something.")})();
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')  
 
 module.exports = {
   mode: 'development',
@@ -20,5 +21,12 @@ module.exports = {
         ],
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack App',
+      filename: 'index.html',
+      template: 'src/teamplate.html',
+    })
+  ]
 }
